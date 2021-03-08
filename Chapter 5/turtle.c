63a6aafb6b53ea4b9c9eb67eb0a47d9eff62f618
baeg0pa <65937181+baeg0pa@users.noreply.github.com>
2021-03-08 16:07:34 +0900

    Add files via upload

---
+++ Chapter 5/turtle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+typedef struct {
+	const char *name;
+	const char *species;
+	int age;
+}turtle;
+
+void happy_birthday(turtle t)
+{
+	t.age = t.age + 1;
+	printf("생일 축하해 %s! 이제 %i살이 되었네!\n", t.name, t.age);
+}
+
+int main()
+{
+	turtle myrtle = {"꼬부기", "장수거북", 99};
+	happy_birthday(myrtle);
+	printf("%s의 나이는 이제 %i살입니다.\n", myrtle.name, myrtle.age);
+}
